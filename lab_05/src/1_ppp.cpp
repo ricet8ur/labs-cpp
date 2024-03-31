57e7a76b50f01459171d1eabbe472dbe88d4b95f
ricet8ur <redcma@yandex.com>
2024-03-31 10:56:51 +0300

    lab_05: wrong architecture

--- lab_05/src/1_ppp.cpp
+++ lab_05/src/1_ppp.cpp
@@ -6,6 +6,7 @@
 #include <deque>
 #include <functional>
 #include <future>
+#include <atomic>
 using namespace std;
 
 class ppp;
@@ -18,21 +19,25 @@ using fvf = function<vector<fwrapper>()>;
 template <typename T>
 class expression
 {
-    bool _calculated;
+    atomic<bool> _calculated{0};
     T _result;
     function<T()> f;
 
 public:
     ppp &context;
-    vector<expression<T>> next_exprs;
-    vector<expression<T>> prev_exprs;
+    vector<expression<T> *> next_exprs;
+    vector<expression<T> *> prev_exprs;
 
-    expression(ppp &context, function<T()> f) : _calculated{false}, f{f}, context{context}, next_exprs{} {}
-    expression(ppp &context, function<T()> f, T result) : _calculated{true}, _result{result}, f{f}, context{context}, next_exprs{} {}
-
-    const bool is_calculated()
+    expression(const expression &other) : f{other.f}, context{other.context}, next_exprs{}
     {
-        return _calculated;
+        _calculated.store(other.is_calculated());
+    }
+    expression(ppp &context, function<T()> f) : f{f}, context{context}, next_exprs{} { _calculated.store(false); }
+    expression(ppp &context, function<T()> f, T result) : _result{result}, f{f}, context{context}, next_exprs{} { _calculated.store(false); }
+
+    bool is_calculated() const
+    {
+        return _calculated.load();
     }
 
     T result()
@@ -42,11 +47,21 @@ public:
 
     fwrapper get_calculator()
     {
-        return fwrapper{fvf{[&]() mutable -> vector<fwrapper>
-                            {
-                                             _result = f();
-                                             _calculated = true;
-                                             return vector<fwrapper>{get_new_calculators()}; }}};
+        return fwrapper
+        {
+            fvf
+            {
+                [&]() mutable -> vector<fwrapper>
+                {
+                if (is_calculated())
+                    return vector<fwrapper>{};
+                else{
+                             _result = f();
+                    cout<<_result<<'a'<<std::this_thread::get_id()<<endl;
+                             _calculated.store(true);
+                             return vector<fwrapper>{get_new_calculators()}; } }
+            }
+        };
     }
 
     fwrapper get_new_calculators()
@@ -54,15 +69,19 @@ public:
         return fwrapper{fvf{[&]() mutable -> vector<fwrapper>
                             {
             vector<fwrapper> new_calculators;
-            for (expression<T> &next_expr : next_exprs)
+            for (expression<T> *next_expr : next_exprs)
             {
                 // check next expression is ready to be calculated
                 bool pre = true;
-                for (expression<T> &pre_expr : next_expr.prev_exprs)
-                    pre = pre && pre_expr.is_calculated();
-                if (pre)
-                    new_calculators.emplace_back(fwrapper{next_expr.get_calculator()});
+                for (expression<T> *pre_expr : next_expr->prev_exprs){
+                    pre = pre && pre_expr->is_calculated();
+
+                }
+                if (pre&&!next_expr->prev_exprs.empty())
+                    new_calculators.emplace_back(next_expr->get_calculator());
+
             }
+                cout<<_result<<'g'<<new_calculators.size()<<endl;
             return new_calculators; }}};
     }
 };
@@ -70,12 +89,14 @@ template <typename T>
 auto operator+(expression<T> &lhs, expression<T> &rhs)
 {
     auto exp = expression(
-        lhs.context, function<T()>([lhs, rhs]() mutable -> T
-                                   { return lhs.result() + rhs.result(); }));
-    exp.prev_exprs.emplace_back(lhs);
-    exp.prev_exprs.emplace_back(rhs);
-    lhs.next_exprs.emplace_back(exp);
-    rhs.next_exprs.emplace_back(exp);
+        lhs.context, function<T()>([&lhs, &rhs]() mutable -> T
+                                   { 
+                                    cout<<'d'<<endl;
+                                    return lhs.result() + rhs.result(); }));
+    exp.prev_exprs.emplace_back(&lhs);
+    exp.prev_exprs.emplace_back(&rhs);
+    lhs.next_exprs.emplace_back(&exp);
+    rhs.next_exprs.emplace_back(&exp);
     cout << exp.context.q.size() << ' ';
     return exp;
 }
@@ -94,9 +115,11 @@ public:
     auto add_variable(const T t)
     {
         auto exp = expression(
-            *this, function([t]() -> T
-                            { return t; }),
-            t);
+            *this, function<T()>([t]()mutable -> T
+                            { 
+                                    cout<<'p'<<endl;
+                                return t; }),
+            23.);
         exp.context.q.push_back(exp.get_calculator());
         return exp;
     }
@@ -107,17 +130,25 @@ public:
         auto futures2 = deque<future<vector<fwrapper>>>{};
         while (!q.empty())
         {
+            cout << q.size() << 'w' << endl;
+
             // start read-write parallel wrap
             for (auto &&c : q)
             {
                 futures.emplace_back(async(c.v));
             }
+            cout << q.size() << 'b' << endl;
+
             q.clear();
             // parallel read-only preparation wrap
-            for (auto &&fv : futures)
+            for (auto &fv : futures)
             {
-                for (auto &&fw : fv.get())
+                auto v = fv.get();
+                cout << q.size() << v.size() << 'c' << endl;
+                for (auto &fw : v)
+                {
                     futures2.emplace_back(async(fw.v));
+                }
             }
             futures.clear();
 
